b3337485e7b32da0f69baeb2fabe991046de5aa7
Martijn Dorsman <martijn.dorsman@gmail.com>
2022-09-24 20:20:23 +0200

    wip

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Event;
+use App\Models\EventUser;
 use App\Models\Expense;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -16,25 +17,45 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $user = User::factory()
-            ->has(
-                Event::factory()
-                    ->count(3)
-                    ->has(
-                        Expense::factory()
-                            ->count(10)
-                            ->has(
-                                User::factory()->count(5),
-                                'participants'
-                            ),
-                    )
-                    ->state(function ($attributes, User $user) {
-                        return ['user_id' => $user->id];
-                    })
-            )
-            ->create([
-                'email' => 'martijn.dorsman@gmail.com',
-                'name' => 'Martijn Dorsman'
-            ]);
+        // Create admin user
+        $user = User::factory()->create([
+            'email' => 'martijn.dorsman@gmail.com',
+            'name' => 'Martijn Dorsman'
+        ]);
+
+        // Create 5 users
+        $users = collect([$user, ...User::factory(4)->create()]);
+
+        // Create an event and attach 5 users to it
+        $event = Event::factory()
+            ->create(['user_id' => $user->id]);
+        $event->users()->attach($users->pluck('id'));
+        $expenses = Expense::factory(5)
+            ->sequence(fn ($sequence) => ['user_id' => $users[$sequence->index]->id])
+            ->create(['event_id' => $event->id]);
+
+        // Add all users as participants to the expenses
+        $expenses->each(fn ($expense) => $expense->participants()->attach($users->pluck('id')));
+
+        // $user = User::factory()
+        //     ->has(
+        //         Event::factory()
+        //             ->count(3)
+        //             ->has(
+        //                 Expense::factory()
+        //                     ->count(10)
+        //                     ->has(
+        //                         User::factory()->count(5),
+        //                         'participants'
+        //                     ),
+        //             )
+        //             ->state(function ($attributes, User $user) {
+        //                 return ['user_id' => $user->id];
+        //             })
+        //     )
+        //     ->create([
+        //         'email' => 'martijn.dorsman@gmail.com',
+        //         'name' => 'Martijn Dorsman'
+        //     ]);
     }
 }
--- resources/views/pages/events/show.blade.php
+++ resources/views/pages/events/show.blade.php
@@ -1,8 +1,14 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Event: {{ $event->name }}
-        </h2>
+        <div class="flex justify-between items-center px-4">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Event: {{ $event->title }}
+            </h2>
+            <div>
+                <h2 class="mt-2 font-semibold text-xl">Created by</h2>
+                {{ $event->creator->name }}
+            </div>
+        </div>
     </x-slot>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -12,25 +18,34 @@
                         <h2>Closed at {{ $event->ended_at?->format('d-m-Y') }}</h2>
                     @endif
 
-                    <h2 class="mt-2 font-semibold">Members</h2>
-                    <ul>
+                    <h2 class="mt-2 font-semibold text-xl">Expenses</h2>
+                    <div>
 
-                        @foreach ($event->users ?? [] as $user)
-                            <li>{{ $user->name }}</li>
-                        @endforeach
-                    </ul>
-                    <h2 class="mt-2 font-semibold">Expenses</h2>
-                    <ul>
-                        @foreach ($event->expenses as $expense)
-                            <li>{{ $expense->title }}, € {{ number_format($expense->amount / 100, 2, ',', '.') }}</li>
-                        @endforeach
                         @if ($event->expenses->count() === 0)
                             <em>No expenses.</em>
+                        @else
+                            <div class="divide-y">
+                                <div class="grid grid-cols-12 my-2">
+                                    <div class="col-span-2 font-semibold">Payer</div>
+                                    <div class="col-span-2 font-semibold">Title</div>
+                                    <div class="col-span-2 font-semibold">Amount</div>
+                                    <div class="col-span-2 font-semibold">Date</div>
+                                    <div class="col-span-4 font-semibold">Participants</div>
+                                </div>
+                                @foreach ($event->expenses as $expense)
+                                    <div class="grid grid-cols-12 py-8 text-sm">
+                                        <div class="col-span-2">{{ $expense->payer->name }}</div>
+                                        <div class="col-span-2">{{ $expense->title }}</div>
+                                        <div class="col-span-2">€
+                                            {{ number_format($expense->amount / 100, 2, ',', '.') }}</div>
+                                        <div class="col-span-2">{{ $expense->created_at->format('d-m-y') }}</div>
+                                        <div class="col-span-4">{{ $expense->participants->map->name->join(', ') }}
+                                        </div>
+                                    </div>
+                                @endforeach
+                            </div>
                         @endif
-                    </ul>
-
-                    <h2 class="mt-2 font-semibold">Created by</h2>
-                    {{ $event->creator->name }}
+                    </div>
                 </div>
             </div>
         </div>
