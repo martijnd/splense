07de266bf717b6c091939556fa23cd7181766f60
Martijn Dorsman <martijn.dorsman@gmail.com>
2022-09-28 21:38:22 +0200

    wip

--- database/migrations/2022_09_24_172850_create_expense_user_table.php
+++ database/migrations/2022_09_24_172850_create_expense_user_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->id();
             $table->foreignIdFor(User::class)->constrained();
             $table->foreignIdFor(Expense::class)->constrained();
-            $table->timestamps();
         });
     }
 
